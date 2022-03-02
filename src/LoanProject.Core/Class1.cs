689b7b64fad06f374d46b2faee505aac858eafdd
azangaladze <a.zangaladze@gmail.com>
2022-03-02 23:28:44 +0400

    Add project files.

---
+++ src/LoanProject.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LoanProject.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/LoanProject.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LoanProject.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
